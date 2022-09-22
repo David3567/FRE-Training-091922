ac06c7494b1c48f95dfbc5df936eb8175a4bd391
Yue Dong <ydong011@fiu.edu>
2022-09-22 12:59:42 -0500

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+console.log("hello");
+/**
+ * @class
+ * what is javascript, ECMAScript
+ * javascript vs. nodejs vs. ECMAScript
+ *
+ * Primitive Data
+ * Object Data or reference Data
+ *
+ * coercion
+ * equality == vs. ===
+ *
+ * var vs. let vs. const
+ *
+ * oop: Object oriented programming in JS
+ * encapsulation; inheritance; Poly-morph-ism; abstraction;
+ * constructer function, prototype chain
+ */
