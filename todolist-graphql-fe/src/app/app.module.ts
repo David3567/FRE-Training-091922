86a1ea3cfb304d18adfc336e60deb499a0e9fadf
Yue Dong <ydong011@fiu.edu>
2022-11-17 14:05:48 -0600

    update

--- todolist-graphql-fe/src/app/app.module.ts
+++ todolist-graphql-fe/src/app/app.module.ts
@@ -29,3 +29,19 @@ import { TodolistComponent } from './todolist/todolist.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* 
+  & install graphql
+  $ npm install apollo-angular @apollo/client graphql
+
+  & add graphql.module.ts and change the url to your target url
+
+  & add GraphQLModule to app.module.ts
+
+  & install graphql generator
+  $ npm i --save-dev @graphql-codegen/cli @graphql-codegen/typescript @graphql-codegen/typescript-apollo-angular @graphql-codegen/typescript-operations
+
+  & add codegen.yml to root folder
+  
+  & add script to package.json: "codegen": "gql-gen"
+*/
